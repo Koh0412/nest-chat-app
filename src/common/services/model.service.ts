398e8fd89204f1fb7cfd8efbde524d75b9722eba
kohei.hujita <point.out92@gmail.com>
2020-06-11 01:35:11 +0900

    add base model service class

---
+++ src/common/services/model.service.ts
@@ -0,0 +1,39 @@
+import { Injectable } from "@nestjs/common";
+import { InjectRepository } from "@nestjs/typeorm";
+import { Repository, BaseEntity, InsertResult } from "typeorm";
+
+/**
+ * serviceクラスの基底クラス
+ */
+@Injectable()
+export class ModelService<T> {
+  constructor(@InjectRepository(BaseEntity) private readonly repository: Repository<T>) {}
+
+  /**
+   * 与えられたentityをdb内に追加
+   *
+   * @param data
+   */
+  async create(data: T): Promise<InsertResult> {
+    return this.repository.insert(data);
+  }
+
+  /**
+   * 全てのentityを返す
+   */
+  async all(): Promise<T[]> {
+    return this.repository.find();
+  }
+
+  /**
+   * 指定したidのentityを取得
+   *
+   * @param id
+   */
+  async findOne(id: string): Promise<T> {
+    const model = this.repository.findOne({
+      where: { id: parseInt(id, 10) }
+    });
+    return model;
+  }
+}
--- src/modules/users/users.service.ts
+++ src/modules/users/users.service.ts
@@ -1,24 +1,12 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { User } from 'src/entity/user.entity';
-import { Repository, InsertResult } from 'typeorm';
+import { Repository } from 'typeorm';
+import { ModelService } from 'src/common/services/model.service';
 
 @Injectable()
-export class UsersService {
-  constructor(@InjectRepository(User) private readonly userRepository: Repository<User>) {}
-
-  async create(data: Partial<User>): Promise<InsertResult> {
-    return this.userRepository.insert({ ...data });
-  }
-
-  async all(): Promise<User[]> {
-    return this.userRepository.find();
-  }
-
-  async findOne(id: string): Promise<User> {
-    const user = this.userRepository.findOne({
-      where: { id: parseInt(id, 10) }
-    });
-    return user;
+export class UsersService extends ModelService<User> {
+  constructor(@InjectRepository(User) private readonly userRepository: Repository<User>) {
+    super(userRepository);
   }
 }
