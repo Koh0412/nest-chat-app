624352b94d1837194cee7c95922f02f0682a1151
kohei.hujita <point.out92@gmail.com>
2020-06-13 16:21:10 +0900

    change view engine from ejs to nunjucks

--- src/main.ts
+++ src/main.ts
@@ -2,13 +2,31 @@ import { NestFactory } from '@nestjs/core';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { join } from "path";
 import { AppModule } from './domains/app/app.module';
+import * as nunjucks from "nunjucks";
+import * as helmet from "helmet"
 
 async function bootstrap() {
-  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule, {
+    logger: ["error", "warn"]
+  });
 
+  const nunjucksOptions: nunjucks.ConfigureOptions = {
+    autoescape: true,
+    throwOnUndefined: false,
+    trimBlocks: false,
+    lstripBlocks: false,
+    watch: true,
+    noCache: process.env.NODE_ENV === "local" ? true : false,
+    express: app
+  };
+
+  app.use(helmet());
   app.useStaticAssets(join(__dirname, "..", "public"));
   app.setBaseViewsDir(join(__dirname, "..", "views"));
-  app.setViewEngine("ejs");
+
+  const viewEnv = nunjucks.configure("views", nunjucksOptions);
+  app.engine("njk", viewEnv.render);
+  app.setViewEngine("njk");
 
   await app.listen(3000);
 }
