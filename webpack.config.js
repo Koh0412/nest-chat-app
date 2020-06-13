4a548cb40224ba483d477cfc11e70ce200b1feb4
kohei.hujita <point.out92@gmail.com>
2020-06-14 05:21:19 +0900

    add webpack for client code

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/client/app.ts',
+  watchOptions: {
+    ignored: ["node_modules", "./src/server"],
+  },
+  output: {
+    filename: 'app.js',
+    path: path.join(__dirname, './public/js'),
+  },
+  resolve: {
+    extensions: ['.ts', '.js'],
+  },
+  // module: {
+  //   rules: [
+  //     {
+  //       test: /\.ts$/, loader: 'ts-loader'
+  //     }
+  //   ]
+  // }
+};
