22f012765dc1a7bc9927abb5a606952c019cd811
kohei.hujita <point.out92@gmail.com>
2020-06-20 20:05:49 +0900

    fix top view form submit and refactoring

--- src/server/domains/app/app.controller.ts
+++ src/server/domains/app/app.controller.ts
@@ -1,8 +1,8 @@
-import { Controller, Get, UseGuards, Post, Request, UseFilters, Req, Res } from "@nestjs/common";
+import { Controller, Get, UseGuards, Post, Request, UseFilters, Req, Redirect } from "@nestjs/common";
 import { View, AuthGet } from "src/server/common/decorators";
 import { LoginGuard } from "src/server/handlers/guards";
 import { AuthExceptionFilter } from "src/server/handlers/filters";
-import { Request as ExpressRequest, Response } from "express";
+import { Request as ExpressRequest } from "express";
 
 @Controller()
 @UseFilters(AuthExceptionFilter)
@@ -13,10 +13,17 @@ export class AppController {
     //
   }
 
+  @Post()
+  @Redirect("back")
+  async postMessage() {
+    //
+  }
+
   @UseGuards(LoginGuard)
   @Post("login")
-  async login(@Res() res: Response) {
-    return res.redirect("/me");
+  @Redirect("me")
+  async login() {
+    //
   }
 
   @Get("login")
